0dda23905ee6b790a0beabacc8c8103e10eaea94
fkfouri <fkfouri@d52c3466-c1fc-5542-8b25-f72b27a845b4>
2018-10-25 12:08:18 +0000

    Codigos de Consumer e Producer de comunicacao com o Kafka
    git-svn-id: http://cockpitalm.emb/svn/CockpitDesk/ApoioCockpit/Kafka@4024 d52c3466-c1fc-5542-8b25-f72b27a845b4

---
+++ Consumer/Program_consumer.cs
@@ -0,0 +1,39 @@
+﻿using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    class Program_consumer
+    {
+        static void Main(string[] args)
+        {
+            var options = new KafkaOptions(new Uri("http://sjkap556:9092"), new Uri("http://sjkap556:9092"));
+            var router = new BrokerRouter(options);
+
+            OffsetPosition[] offsetPositions = new OffsetPosition[]
+                 {
+                        new OffsetPosition()
+                        {
+                           Offset = 0,
+                           PartitionId = 0
+                        }
+                 };
+
+            var consumer = new KafkaNet.Consumer(new ConsumerOptions("testCockpit", new BrokerRouter(options)), offsetPositions);
+
+
+            //Consume returns a blocking IEnumerable (ie: never ending stream)
+            foreach (var message in consumer.Consume())
+            {
+                Console.WriteLine("Response: P{0},O{1} : {2}", message.Meta.PartitionId, message.Meta.Offset, Encoding.UTF8.GetString(message.Value));
+            }
+
+        }
+    }
+}
---
+++ Producer/Program_Producer.cs
@@ -0,0 +1,29 @@
+﻿using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Producer
+{
+    class Program_Producer
+    {
+        static void Main(string[] args)
+        {
+            var options = new KafkaOptions   (new Uri("http://sjkap556:9092"));
+            var router = new BrokerRouter(options);
+
+            var client = new KafkaNet.Producer(router);
+
+            for (int i = 0; i < 1; i++)
+            {
+                client.SendMessageAsync("testCockpit", new[] { new Message(DateTime.Now + " -- Teste: " + i) }).Wait();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
